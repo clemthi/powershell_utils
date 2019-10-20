e1ca9c2a1c8dcbf6af2edeb0092455ad3261106e
Clem T <30405704+clemthi@users.noreply.github.com>
2019-10-20 18:17:16 +0200

    delete & rename stuff

---
+++ del_stuff_regex.ps1
@@ -0,0 +1,22 @@
+$sourceDir = "c:\path\to\dir"
+$searchPattern = ".*\(.*\).*"
+$folderOnly = $false # folder or file, not both
+$testOnly = $false
+
+$itemList = Get-ChildItem -Path $sourceDir -Recurse -Force | Where-Object { ($_.Name -Match $searchPattern) }
+if ($folderOnly) {
+    $itemList = $itemList.Where{ ($_.PSIsContainer) }
+}
+else {
+    $itemList = $itemList.Where{ (! $_.PSIsContainer) }
+}
+
+foreach ($item in $itemList) {
+    if ($testOnly) {
+        Remove-Item $obj.FullName -Recurse -Force -WhatIf
+    }else {
+        Remove-Item $obj.FullName -Recurse -Force
+    }
+}
+
+"Items deleted: " + $itemList.Count
---
+++ rename_stuff_regex.ps1
@@ -0,0 +1,26 @@
+﻿$sourceDir = "c:\path\to\dir"
+$searchPattern = ".*\(.*\).*"
+$replacePattern = " \(.*\)" # pattern of the name to be replaced
+$replacePatternBy = ""
+$folderOnly = $false # folder or file, not both
+$testOnly = $false
+
+$itemList = Get-ChildItem -Path $sourceDir -Recurse -Force | Where-Object { ($_.Name -Match $searchPattern) }
+if ($folderOnly) {
+    $itemList = $itemList.Where{ ($_.PSIsContainer) }
+}
+else {
+    $itemList = $itemList.Where{ (! $_.PSIsContainer) }
+}
+
+foreach ($item in $itemList) {
+    $itemPath = Split-Path -Path $item.FullName
+    $newItemFullName = $itemPath + "\" + ($item.Name -Replace $replacePattern, $replacePatternBy)
+    if ($testOnly) {
+        Rename-Item $file.FullName $newItemFullName -WhatIf
+    }else {
+        Rename-Item $file.FullName $newItemFullName
+    }
+}
+
+"Items renamed: " + $itemList.Count
